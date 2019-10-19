3875435c65744f7b6e606b342fa10460d6e3fc98
Percy <percival.ngan@gmail.com>
2019-10-19 09:28:37 -0500

    Added main and table views. Subclassed the views to their VC.

---
+++ ios-studentsList2/StudentsList/StudentsList/Resources/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudentsList
+//
+//  Created by Percy Ngan on 10/18/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
