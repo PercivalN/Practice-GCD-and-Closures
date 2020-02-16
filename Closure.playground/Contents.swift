ef485da3888de9a7b47fa03519575cefee98629a
Percival N <percival.ngan@gmail.com>
2020-02-16 00:44:36 -0600

    Added closure examples

--- Closure.playground/Contents.swift
+++ Closure.playground/Contents.swift
@@ -1,7 +1,41 @@
 import UIKit
 
-let myClosure = {
-	print("Hello World")
-}
+//func runThis(closure: () -> ()) {
+//	closure()
+//}
+//
+//let closure = {
+//	print(1)
+//	print(10)
+//	print(100)
+//}
+//
+//runThis(closure: closure)
 
-myClosure()
+//func runTime(closure: () -> ()) {
+//	closure()
+//}
+//
+//runTime() {
+//	print(42)
+//}
+
+// -------------------------------------------------------
+//let closure = { (a: Int, b: Int) -> Int in
+//	return a + b
+//}
+//
+//print(closure(2,3))
+
+let closure = { (string: String, repeatCount: Int) -> String in
+
+	 var text = ""
+	for index in 1...repeatCount {
+		text = ("This is the \(index) time \(string) is printed.")
+		print(text)
+	}
+	return text
+}
+ closure("Hello World", 3)
+
+
